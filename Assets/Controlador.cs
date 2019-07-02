9188ad146dcc2401ec0a535417251c0c89ed22e8
Paulo Salvatore <paulo@salvatore.tech>
2019-07-02 18:24:37 -0300

    Initial commit

---
+++ Assets/Controlador.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour
+{
+	// Use this for initialization
+	private void Start()
+	{
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		var controller = OVRInput.GetActiveController();
+
+		if (Input.GetMouseButtonDown(0) || OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, controller))
+		{
+		}
+	}
+}
